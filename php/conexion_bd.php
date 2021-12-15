9a095294e1c98797b6fbdb4139d6bf74b2193958
AndresHernandezdev <andres.hernandez.nahm@gmail.com>
2021-12-15 17:58:32 -0300

    se agrega registo PHP

---
+++ php/conexion_bd.php
@@ -0,0 +1,13 @@
+<?php
+
+   // $conexion = mysqli_connect("https://201.148.105.104/phpmyadmin/", "scrumsales_dev", "2Z4w6USXZ", "registro_cliente");
+    $conexion = mysqli_connect("localhost", "root", "", "scrumsales_dev");
+
+
+    if ($conexion){
+        echo "Conectado Exitosamente";
+    }else{
+        echo "Error de conexion";
+    }
+
+?>
---
+++ php/registro_cliente.php
@@ -0,0 +1,22 @@
+<?php 
+
+    include 'conexion_bd.php';
+
+
+    /** ASIGNA VARIABLES DE RECGISTRO CLIENTE */
+    $nombre = $_POST['Nombre'];
+    $apellido = $_POST['Apellido'];
+    $correo = $_POST['Correo'];
+    $telefono = $_POST['Telefono'];
+    $clave = $_POST['Contraseña'];
+    
+    /* INSERTA REGISTROS EN TABLA DE BASE DE DATOS */
+    $query = "INSERT INTO registro_cliente (nombre, apellido, correo, telefono, clave) 
+              VALUES('$nombre', '$apellido', '$correo', '$telefono', '$clave')";
+    
+    $ejecutar = mysqli_query($conexion, $query);
+
+
+
+
+?>
