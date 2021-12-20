d53588e2a8811af2541d162cf339b0c8fe48c1a4
Sebastián Poblete <sebapoba.dev@gmail.com>
2021-12-20 14:30:28 -0300

    se agrego alerta al registrarse con exito

---
+++ js/registro.js
@@ -0,0 +1,8 @@
+document.getElementById("btn_registrarse").addEventListener("click", register);
+
+
+// var Form_register = document.querySelector(".Form_register");
+
+function register(){
+ console.log("funciona");
+}
